64b893051484327c988ae0966d1ffd703dbaca99
hostdog01 <hostdog01@naver.com>
2023-08-22 16:46:16 +0000

    add api path

--- src/main/java/com/example/kakao/sample/SampleRestController.java
+++ src/main/java/com/example/kakao/sample/SampleRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api") 
 public class SampleRestController {
 
     private final SampleDataRepository sampleDataRepository;
