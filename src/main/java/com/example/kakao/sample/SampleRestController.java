b9961275400b4877d93cb556b73c6836bae04487
hostdog01 <hostdog01@naver.com>
2023-08-22 16:48:57 +0000

    add import

--- src/main/java/com/example/kakao/sample/SampleRestController.java
+++ src/main/java/com/example/kakao/sample/SampleRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
